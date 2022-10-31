8c00e12913fe3a578113b4dfd5dac1834a7449df
Sergio Aguilar <sergioaguilar.ti@outlook.com>
2022-10-30 22:48:43 -0500

    semana 1
    semana 1

--- index.js
+++ index.js
@@ -1,19 +1,53 @@
 const express = require("express");
-const { getEmployees } = require('./services/index');
+const { getEmployees, getEmployeesPaginated, getEmployeesOldest, getEmployeesPrivileges, postEmployees, getEmployeesBadges, getEmployeesByName } = require('./services/index');
 
 const app = express();
 
 app.use(express.json());
 
 app.get('/', (req, res)=> {
-    // res.send('<h1>hello!</h1>');
     res.json({message: 'hello!'});
 });
 
 app.get('/api/employees', (req, res)=> {
-    res.json(getEmployees());
+    if(req.query.page){
+        // http://localhost:8000/api/employees?page=1
+        // http://localhost:8000/api/employees?page=2
+        // http://localhost:8000/api/employees?page=N
+        res.json(getEmployeesPaginated(req.query.page));
+    } else if(req.query.user) {
+        // http://localhost:8000/api/employees?user=true
+        res.json(getEmployeesPrivileges(req.query.user));
+    } else if(req.query.badges) {
+        // http://localhost:8000/api/employees?badges=black
+        res.json(getEmployeesBadges(req.query.badges));
+    } else {
+        // http://localhost:8000/api/employees
+        res.json(getEmployees());
+    }
 });
 
-app.listen(3001, ()=>{
+// http://localhost:8000/api/employees/oldest
+app.get('/api/employees/oldest', (req, res)=> {
+    res.json(getEmployeesOldest());
+});
+
+// http://localhost:8000/api/employees
+app.post('/api/employees', (req, res)=> {
+    if(postEmployees(req.body).code){
+        res.status(400).json(postEmployees(req.body));
+    }
+    res.json(postEmployees(req.body));
+});
+
+// http://localhost:8000/api/employees/Bob
+app.get('/api/employees/:name', (req, res)=> {
+    if(getEmployeesByName(req.body).code){
+        res.status(404).json(getEmployeesByName(req.params.name));
+    }
+    res.json(getEmployeesByName(req.params.name));
+});
+
+app.listen(8000, ()=>{
     console.log('Ready!');
 });
--- services/index.js
+++ services/index.js
@@ -1,13 +1,80 @@
-const Employess = require('../data/employees.json');
-
-// () => {
-//     console.log(employess);
-// }
+const Employees = require('../data/employees.json');
 
 const getEmployees = () => {
-    return Employess;
+    return Employees;
+}
+
+const getEmployeesPaginated = (page) => { 
+    const intoPage = parseInt(page);
+    const limit = 2;
+
+    const startIndex = (intoPage - 1)*limit;
+    const endIndex = intoPage*limit;
+    const results = {};
+
+    if ( endIndex < Employees.length ) {
+        results.next = {
+            intoPage: intoPage + 1,
+            limit: limit
+        }
+    }
+
+    if (startIndex > 0) {
+        results.previous = {
+            intoPage: intoPage - 1,
+            limit: limit
+        }
+    }
+
+    results.results = Employees.slice(startIndex, endIndex);
+    return results;
+}
+
+const getEmployeesOldest = () => {
+    const age = Employees.map( data => data.age );
+    const maxAge = Math.max(...age);
+    const oldest = Employees.find( data => data.age === maxAge );
+    return oldest;
+}
+
+const getEmployeesPrivileges = (privileges) => {
+    const valor = privileges === 'true' ? 'user' : 'admin';
+    const results = Employees.filter( data => data.privileges === valor);
+    return results;
+}
+
+const postEmployees = (body) => {    
+    const keys = Object.keys(Employees[0]);
+    const receiveKeys = Object.keys(body);
+    // console.log()
+    if (keys.length === receiveKeys.length && keys.every(value => ~receiveKeys.indexOf(value))) {
+        Employees.push(body);
+        return Employees;
+    } else { 
+        return {"code": "bad_request"};
+    }
+}
+
+const getEmployeesBadges = (badge) => {
+    const data = Employees.filter(data => data.badges.includes(badge));
+    return data;
+}
+
+const getEmployeesByName = (name) => {
+    const data = Employees.filter(data => data.name === name);
+    if ( data.length > 0 ) {
+        return data;
+    } else { 
+        return {"code": "bad_request"};
+    }
 }
 
 module.exports = {
-    getEmployees 
+    getEmployees,
+    getEmployeesPaginated,
+    getEmployeesOldest,
+    getEmployeesPrivileges,
+    postEmployees,
+    getEmployeesBadges,
+    getEmployeesByName
 };
